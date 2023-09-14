225b2a24900e776a377cd3a9a06c557337d048a3
Platosdog <62915572+Platosdog@users.noreply.github.com>
2023-09-14 10:27:54 +0900

    routing enabled links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TestComponent } from './test/test.component';
+import { PlayComponent } from './play/play.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'test', component: TestComponent },
+  { path: 'play', component: PlayComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
