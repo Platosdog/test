d2780acea0b5de4d8c8ecfcc1af946553fbcac7b
Platosdog <62915572+Platosdog@users.noreply.github.com>
2023-09-15 08:42:06 +0900

    no stand alone

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,15 +1,9 @@
 import {Component} from '@angular/core';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatNativeDateModule} from '@angular/material/core';
 
 @Component({
-  standalone: true,
   selector: 'app-test',
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css'],
-  imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule],
 })
 export class TestComponent {
 
