9989ea1f083760ead44911811782088e5736ff86
Platosdog <62915572+Platosdog@users.noreply.github.com>
2023-09-22 14:00:16 +0900

    Working on routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TestComponent } from './test/test.component';
-import { PlayComponent } from './play/play.component';
 import { Datepicker } from './Datepicker/datepicker';
 import { VacationsComponent } from './Vacations/vacations.component'
 
 const routes: Routes = [
-  { path: 'test', component: TestComponent },
-  { path: 'play', component: PlayComponent },
   { path: 'datepicker', component: Datepicker},
   { path: 'vacations', component: VacationsComponent },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
-import { PlayComponent } from './play/play.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { VacationsComponent } from './Vacations/vacations.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent,
-    PlayComponent,
     VacationsComponent
   ],
   imports: [
